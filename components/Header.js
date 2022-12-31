8c1452f36b29a033b326fdd7289a36d657363b51
Alexander van der Leek <alexandervanderleek@gmail.com>
2022-12-31 12:45:54 +0200

    morefeatures

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
+import { MagnifyingGlassIcon, PlusCircleIcon } from '@heroicons/react/24/outline'
+import {HomeIcon} from '@heroicons/react/24/solid'
 
 export default function Header() {
   return (
-    <div>
-      {/* Left */}
-            <div className="flex items-center justify-between max-w-6xl">
+   
+      
+            <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
                 <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
                     <Image
                         className="object-contain"
@@ -14,7 +16,7 @@ export default function Header() {
                         alt="insta"
                     />
                 </div>
-                <div className="cursor-pointer h-10 w-10 relative lg:hidden">
+                <div className="cursor-pointer h-24 w-10 relative lg:hidden">
                     <Image
                         className="object-contain"
                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
@@ -23,13 +25,22 @@ export default function Header() {
                     />
                 </div>
 
+                <div className="relative mt-1">
+                    <div className="absolute top-2 left-2">
+                        <MagnifyingGlassIcon className='h-5 text-gray-500'></MagnifyingGlassIcon>
+                    </div>
 
-                <h1>Right side</h1>
+                    <input type="text" placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md' />
+                </div>
+
+                <div className="flex space-x-4 items-center">
+                    <HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'></HomeIcon>
+                    <PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out' ></PlusCircleIcon>
+                    <img src="https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg" alt="user-image" className='h-10 w-10 rounded-full cursor-pointer' />
+                </div>
 
             </div>
-      {/* Mid */}
-
-      {/* Right */}
-    </div>
+     
+    
   )
 }
